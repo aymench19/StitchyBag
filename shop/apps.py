294da428bf3b9f1db17ebd159c8c38e65584400f
aymench19 <mnchairat@gmail.com>
2026-06-10 23:18:25 +0100

    update site

--- shop/apps.py
+++ shop/apps.py
@@ -1,6 +1,18 @@
 from django.apps import AppConfig
-
+import os
 
 class ShopConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'shop'
+
+    def ready(self):
+        from django.contrib.auth.models import User
+
+        username = os.getenv("DJANGO_ADMIN_USERNAME")
+        password = os.getenv("DJANGO_ADMIN_PASSWORD")
+
+        if username and not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(
+                username=username,
+                password=password
+            )
